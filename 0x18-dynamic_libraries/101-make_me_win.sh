de77dce6d967ad5a36f05d80c82bc0d4deb78995
KariMaa-MahMoud <128936629+KariMaa-MahMoud@users.noreply.github.com>
2023-12-12 09:38:09 +0200

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -P .. https://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@github.com/KariMaa-MahMoud/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so"
