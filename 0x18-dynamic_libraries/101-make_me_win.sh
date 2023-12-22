231628290bfa4490b7e0a65728c41df71932b27d
KariMaa-MahMoud <karima.mahmoud10@gmail.com>
2023-12-22 15:47:15 -0800

    tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /temp https://github.com/alx-low_level_programming/KariMaa-MahMoud/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /temp https://github.com/alx-low_level_programming/KariMaa-MahMoud/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/temp/libgiga.so
